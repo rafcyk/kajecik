87aa7e9c03edf830a91bc621a015405a070b3883
Rafał Dobrowolski <rafcy9@gmail.com>
2020-10-18 20:45:00 +0200

    Updates

---
+++ precache-manifest.8f5aea42b4dcf101ac219f81b7621334.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fdb03f55b5117ad2b5dc5be68d1b7f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21d906c5b73d49bfd0e9",
+    "url": "/static/css/main.e199565f.chunk.css"
+  },
+  {
+    "revision": "cb9d637f2bc095bfd8fb",
+    "url": "/static/js/2.1c43c54b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1c43c54b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21d906c5b73d49bfd0e9",
+    "url": "/static/js/main.aeef161a.chunk.js"
+  },
+  {
+    "revision": "0e306a3bb1e29a91cde8",
+    "url": "/static/js/runtime-main.60e1856e.js"
+  },
+  {
+    "revision": "185dd4a5add951ffb7cd002d2ab716a2",
+    "url": "/static/media/media.185dd4a5.jpg"
+  },
+  {
+    "revision": "12eb9a32f0d9814858c69536e8f792da",
+    "url": "/static/media/notebook.12eb9a32.jpg"
+  },
+  {
+    "revision": "f0063e5042fe4f2472320ed2558fdf5b",
+    "url": "/static/media/poems-back.f0063e50.jpg"
+  },
+  {
+    "revision": "2b27534b50d1568c035dc5742c1e17bc",
+    "url": "/static/media/poems.2b27534b.jpg"
+  }
+]);
